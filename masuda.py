38867b80ebd9f21221343f739e4a3abedf68cdee
Jack Wiseman <jackwiseman4@gmail.com>
2022-01-07 09:06:00 +0000

    fixed box navigation -- only works as long as pokemon have not been picked up

--- masuda.py
+++ masuda.py
@@ -9,61 +9,62 @@ import numpy as np
 from PIL import Image
 import nxbt
 
+# find the position of the cursor in the box by taking a screenshot, converting it
+# to greyscale and checking predetermined slices where the cursor can show up, finding
+# the slice which contains the pixel closest to white
+def get_box_coords(debug=False):
+    img = get_image()
+    img = np.dot(img[...,:3], [.3, .6, .1]) # convert to greyscale
+
+    OFFSET_X = 52 # step size in x direction
+    OFFSET_Y = 62 # step size in y direction
+    pos = (0,0)
+    largest = 0.0 # ensure a result will be found
+
+    # check the party first
+    for row in range(6):
+        party = img[89+(61*row):94+(61*row), 35:42]
+        if(debug):
+            print("(-1, " + str(row) + ") - " + str(party.max()))
+        if(party.max() > largest):
+            largest = party.max()
+            pos = (-1, row)
+ 
+    # iterate over the box
+    # requires "Simple" wallpaper and multiselect on
+    for col in range(6):
+        for row in range(5):
+            candidate = img[95+(OFFSET_Y*row):99+(OFFSET_Y*row), 202+(OFFSET_X*col):208+(OFFSET_X*col)] 
+            if(debug):
+                print("(" + str(col) + ", " + str(row) + ") - " + str(candidate.max()))
+            if(candidate.max() > largest):
+                largest = candidate.max()
+                pos = (col, row)
+    return pos
+
 def saveArrayImage(array, filename):
+    array = array.swapaxes(0,1) 
     as_image = pygame.pixelcopy.make_surface(array)
     filename = 'screenshots/' + filename + '.png'
     pygame.image.save(as_image, filename)
 
-def get_box_coords(debug=False):
-    hand_check = np.array(Image.open("./check-imgs/hand_ref.png"))
-    img = get_image()
-    OFFSET_X = 52
-    OFFSET_Y = 62
-    pos = (0,0)
-    smallest = 200.0 # start with a large "small" value to ensure there will be a smaller one
-
-    # check the party first, as this can save calculations
-    for i in range(5):
-        party = img[87+(61*i):94+(61*i), 35:42, :]
-        if(party.mean() < 175.0):
-            return (-1, i)
- 
-    # iterate over the box, checking for the largest amount of white space
-    # requires "Simple" wallpaper and multiselect on
-    for col in range(6):
-        for row in range(5):
-            candidate = img[95+(OFFSET_Y*row):101+(OFFSET_Y*row), 199+(OFFSET_X*col):208+(OFFSET_X*col), :] 
-            #avg = candidate.mean()
-            img_mse = mse(hand_check, candidate)
-            if(debug):
-                saveArrayImage(candidate, str(col) + "_" + str(row))
-            print("(" + str(col) + ", " + str(row) + ") - " + str(img_mse))
-            if(img_mse < smallest):
-                smallest = img_mse 
-                pos = (col, row)
-    print("Best from this search: " + str(smallest) + " " + str(pos))
-    return pos
-
 def moveTo(dst, nx, controller_index):
-    print("hi")
     curr = get_box_coords()
     print(curr)
     while(get_box_coords()[0] != dst[0]):
-        curr = get_box_coords()
-        print("Adjusting col -- current: " + str(curr[0]) + " DST: " + str(dst[0]))
-        if curr[0] < dst[0]:
+        if get_box_coords()[0] < dst[0]:
             nx.press_buttons(controller_index, [nxbt.Buttons.DPAD_RIGHT], up=1.0)
         else:
             nx.press_buttons(controller_index, [nxbt.Buttons.DPAD_LEFT], up=1.0)
     while(get_box_coords()[1] != dst[1]):
         curr = get_box_coords()
-        print("Adjusting row -- current: " + str(curr[1]) + " DST: " + str(dst[1]))
+        print("Adjusting row -- current: " + str(curr) + " DST: " + str(dst[1]))
         if curr[1] < dst[1]:
             nx.press_buttons(controller_index, [nxbt.Buttons.DPAD_DOWN], up=1.0)
         else:
             nx.press_buttons(controller_index, [nxbt.Buttons.DPAD_UP], up=1.0)
 
-def reset_hunt():
+def masuda():
     stats = None
     if os.path.isfile("stats.json"):
         with open("stats.json", "r") as f:
@@ -89,32 +90,47 @@ def reset_hunt():
     nx.wait_for_connection(controller_index)
 
     add_to_stat_log(stats, "Controller Connected")
-    
-    time.sleep(4)
 
-#    print(get_box_coords())
-    moveTo((4,4), nx, controller_index)
+    time.sleep(5)
 
-    a = True
-    if a:
-        return
+#    # take and save ref screenshots of hands, these dimensions should stay the same as call in getBoxPos()
+#    # outdated, but I might still use later on...
 
+#    OFFSET_X = 52
+#    OFFSET_Y = 62
+#
+#    for i in range(6):
+#        for j in range(6):
+#            saveArrayImage(get_image()[95+(OFFSET_Y*i):99+(OFFSET_Y*i), 202+(OFFSET_X*j):208+(OFFSET_X*j), :],  str(j) + "_" + str(i))
+#            print("Saved " + str(j) + "_" + str(i) + ".png")
+#            if (j < 5):
+#               nx.press_buttons(controller_index, [nxbt.Buttons.DPAD_RIGHT], up=2.0)
+#            else:
+#                nx.press_buttons(controller_index, [nxbt.Buttons.DPAD_DOWN], up=2.0)
+#                for j in range(5):
+#                    nx.press_buttons(controller_index, [nxbt.Buttons.DPAD_LEFT], up=2.0)
+#    return
+##    for i in range(5):
+##        saveArrayImage(get_image()[87+(61*i):94+(61*i), 35:42, :], "-1_" + str(i))
+##        nx.press_buttons(controller_index, [nxbt.Buttons.DPAD_DOWN], up=2.0)
+#
+#
 
     # press X until in menu 
     menu_view = False
     while (menu_view == False):
-        nx.press_buttons(controller_index, [nxbt.Buttons.X], up=5.0)
+        nx.press_buttons(controller_index, [nxbt.Buttons.X], up=1.0)
         img = get_image()
         img_mse = mse(menu_open_check, img)
         add_to_stat_log(stats, f"Menu check MSE: {img_mse}")
-        if img_mse < 50:
+        if img_mse < 51:
             menu_view = True
             break
 
     # press A until in pokemon party
     pokemon_view = False
     while (pokemon_view == False):
-        nx.press_buttons(controller_index, [nxbt.Buttons.A], up=5.0)
+        nx.press_buttons(controller_index, [nxbt.Buttons.A], up=1.0)
         img = relative_crop(get_image(), .975, 1.0, .025, 0.0)
         img_mse = mse(pokemon_menu_check, img)
         add_to_stat_log(stats, f"Pokemon view check MSE: {img_mse}")
@@ -125,7 +141,7 @@ def reset_hunt():
     # press R until in box
     box_view = False
     while (box_view == False):
-        nx.press_buttons(controller_index, [nxbt.Buttons.R], up=5.0)
+        nx.press_buttons(controller_index, [nxbt.Buttons.R], up=1.0)
         img = get_image()[38:56, 0:18, :]
         img_mse = mse(box_view_check, img)
         add_to_stat_log(stats, f"Box view check MSE: {img_mse}")
@@ -136,7 +152,7 @@ def reset_hunt():
     # press Y until multiselect is enabled
     multiselect = False
     while (multiselect == False):
-        nx.press_buttons(controller_index, [nxbt.Buttons.Y], up = 5.0)
+        nx.press_buttons(controller_index, [nxbt.Buttons.Y], up=1.0)
         img = get_image()[0:28, 115:143, :]
         img_mse = mse(multiselect_check, img)
         add_to_stat_log(stats, f"Multiselect check MSE: {img_mse}")
@@ -144,9 +160,23 @@ def reset_hunt():
             box_view = True
             break
 
-    # test movement
-    moveTo((5,4), nx, controller_index)
+    # Move to first pokemon in party
+    moveTo((-1,1), nx, controller_index)
+
+    # Select first pokemon
+    nx.press_buttons(controller_index, [nxbt.Buttons.A], up=2.0)
+    saveArrayImage(get_image(), "oneselected")
+    moveTo((-1,5), nx, controller_index)
+    nx.press_buttons(controller_index, [nxbt.Buttons.A], up=2.0)
+    saveArrayImage(get_image(), "pickedup")
+    moveTo((0,0), nx, controller_index)
+    nx.press_buttons(controller_index, [nx.Buttons.A])
+
+
+    
 
 if __name__ == "__main__":
-    reset_hunt()
-    send_message("Completed")
+    masuda()
+    print("Done")
+#    send_message("Completed")
+
